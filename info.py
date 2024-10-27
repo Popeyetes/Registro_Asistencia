a7ab65262686f67ac472ee44ec66727833a4c95f
Popeyetes <pablrmer0@gmail.com>
2024-10-27 12:09:20 -0600

    Add files via upload

---
+++ info.py
@@ -0,0 +1,166 @@
+# Horarios y materias
+
+horario_iset = {
+    'Monday': {'07:50-08:40': 'Fundamentos de Conmutación y Enrutamiento',
+               '09:10-10:00': 'Fundamentos de Conmutación y Enrutamiento',
+               '10:00-11:40': 'Proyectos de Ingeniería III'},
+    'Tuesday': {'07:50-08:40': 'Fundamentos de Conmutación y Enrutamiento',
+                '09:10-10:00': 'Fundamentos de Conmutación y Enrutamiento',
+                '10:00-10:50': 'Ingles III',
+                '10:50-12:30': 'Circuitos Digitales',
+                '12:30-14:10': 'Electrónica de Potencia'},
+    'Wednesday': {'07:00-08:40': 'Circuitos Eléctricos y Electrónicos',
+                  '09:10-10:50': 'Electiva III',
+                  '10:50-12:30': 'Modelado en Sistemas Dinámicos'},
+    'Thursday': {'07:00-08:40': 'Circuitos Eléctricos y Electrónicos',
+                 '09:10-10:50': 'Ingles III',
+                 '10:50-12:30': 'Electrónica de Potencia',
+                 '12:30-14:10': 'Análisis Crítico de la Actualidad'},
+    'Friday': {'07:50-08:40': 'Modelado en Sistemas Dinámicos',
+               '09:10-10:00': 'Modelado en Sistemas Dinámicos',
+               '10:00-10:50': 'Análisis Crítico de la Actualidad',
+               '11:40-12:30': 'Materia común III',
+               '12:30-14:10': 'Circuitos Digitales'}
+}
+
+profesores_iset = {
+    'Análisis Crítico de la Actualidad': ['Felix Cuadras Ramon Antonio'],
+    'Circuitos Digitales': ['Ochoa Brust Alberto'],
+    'Circuitos Eléctricos y Electrónicos': ['Gonzales Avila Efrain'],
+    'Electiva III': ['Mora Quiñones Uriel',
+                     'García Rebolledo Azael'],
+    'Electrónica de Potencia': ['Valencia Valencia Elias Humberto'],
+    'Fundamentos de Conmutación y Enrutamiento': ['Diaz Hernández Juan Antonio'],
+    'Ingles III': ['Gálvez Martell Alicia Armantina'],
+    'Materia común III': ['Diaz Hernández Juan Antonio',
+                          'Mora Quiñones Uriel',
+                          'Felix Cuadras Ramon Antonio',
+                          'Valencia Valencia Elias Humberto',
+                          'Ochoa Brust Alberto',
+                          'Gonzales Avila Efrain',
+                          'Madrigal Sanchez Jose Rodolfo',
+                          'Gálvez Martell Alicia Armantina'],
+    'Modelado en Sistemas Dinámicos': ['Madrigal Sanchez Jose Rodolfo'],
+    'Proyectos de Ingeniería III': ['Mora Quiñones Uriel']
+    }
+
+horario_im = {
+    'Monday': {
+        '13:20-14:10': 'Matemáticas III',
+        '14:10-15:00': 'Matemáticas III',
+        '16:20-17:10': 'Materia Común 3',
+        '17:10-18:00': 'Circuitos Eléctricos',
+        '18:00-18:50': 'Electrónica Analógica'
+    },
+    'Tuesday': {
+        '13:20-14:10': 'Electrónica Analógica',
+        '14:10-15:00': 'Probabilidad y Estadística',
+        '16:20-17:10': 'Probabilidad y Estadística',
+        '17:10-18:00': 'Matemáticas III',
+        '18:00-18:50': 'Circuitos Eléctricos'
+    },
+    'Wednesday': {
+        '13:20-14:10': 'Legislación y Norma',
+        '14:10-15:00': 'Electrónica Analógica',
+        '16:20-17:10': 'Probabilidad y Estadística'
+    },
+    'Thursday': {
+        '13:20-14:10': 'Inglés III',
+        '14:10-15:00': 'Matemáticas III',
+        '16:20-17:10': 'Matemáticas III',
+        '17:10-18:00': 'Circuitos Eléctricos'
+    },
+    'Friday': {
+        '13:20-14:10': 'Inglés III',
+        '14:10-15:00': 'Legislación y Norma',
+        '16:20-17:10': 'Circuitos Eléctricos'
+    }
+}
+
+profesores_im = {
+    'Matemáticas III': ['Selene Cardenas Rodriguez'],
+    'Circuitos Eléctricos': ['Gabriel Navarro Marquez'],
+    'Electrónica Analógica': ['Estephany Cortes Cardenas'],
+    'Probabilidad y Estadística': ['Xochitl Rosiles'],
+    'Legislación y Norma': ['Miguel Zarate'],
+    'Materia Común 3': [
+        'Gabriel Navarro Marquez',
+        'Estephany Cortes Cardenas',
+        'Selene Cardenas Rodriguez',
+        'Xochitl Rosiles',
+        'Miguel Zarate'
+    ],
+    'Inglés III': ['Gustavo Guzman']
+}
+
+horario_ici = {
+    'Monday': {'07:50-08:40': 'Orientacion Educativa',
+               '09:15-11:00': 'Metodos Numericos',
+               '11:00-13:00': 'Programacion Funcional'},
+    'Tuesday': {'07:00-08:40': 'Sistemas Digitales y Embebidos',
+                '11:00-13:00': 'Interconexion de Redes',
+                },
+    'Wednesday': {'07:00-08:40': 'Estructura de Datos',
+                  '10:00-12:00': 'Metodos Numericos',
+                  "12:00-13:00": "Ingles III",
+                  "13:00-15:00": "Programacion Funcional"},
+    'Thursday': {'07:00-08:40': 'Sistemas Digitales y Embebidos',
+                 '09:15-11:00': 'Ecuaciones Diferenciales',
+                 '11:00-13:00': 'Interconexion de Redes',
+                 '13:00-15:00': 'Ingles III'},
+    'Friday': {'09:15-11:00': 'Ecuaciones Diferenciales',
+               '11:00-12:00': 'Metodos Numericos',
+               '13:00-15:00': 'Estructura de datos',
+               }
+}
+
+profesores_ici = {
+    'Estructura de datos': ['Moran Lopez Luis Eduardo'],
+    'Metodos numericos': ['Sierra Andrade David Alejandro'],
+    'Programacion Funcional': ['Mata Lopez Walter Alexander'],
+    'Interconexion de Redes': ['Diaz Hernandez Juan Antonio'],
+    'Sistemas Digitales y Embebidos': ['Bricio Chapula Carlos Adrian'],
+    'Orientacion Educativa': ['Valdivia Carvajal Dafnis'],
+    'Ingles III': ['Ochoa Zuñiga Oscar Octavio'],
+    "Ecuaciones Diferenciales":["Santiago Hernandez Elizabeth"]
+    }
+
+horario_ime = {
+    'Monday': {
+        '08:00-8:40': 'Ingles',
+        '9:15-10:00': 'Orientación Educativa',
+        '10:00-12:00': 'Ecuaciones Diferenciales',
+        '12:00-14:00': 'Electrónica',
+    },
+    'Tuesday': {
+        '9:15-11:00': 'Métodos Numericos',
+        '11:00-13:00': 'Dinámica',
+        '13:00-15:00': 'Cicuitos 1',
+    },
+    'Wednesday': {
+        '9:15-11:00': 'Tecnologia De Los Materiales',
+        '12:00-14:00': 'Ecuaciones Diferenciales',
+    },
+    'Thursday': {
+        '07:00-8:40': 'Tecnologia De Los Materiales',
+        '10:00-12:00': 'Electrónica',
+        '12:00-14:00': 'Circuitos 1',
+    },
+    'Friday': {
+        '07:00-8:40': 'Ingles',
+        '9:15-11:00': 'Dinámica',
+        '11:00-13:00': 'Métodos Numericos',
+        '13:00-14:00': 'Ecuaciones Diferenciales'
+    }
+}
+
+profesores_ime = {
+    'Ecuaciones Diferenciales': ['Arroyo Ledesma Jaime'],
+    'Métodos Numericos': ['Alcaraz Valencia Pablo Armando'],
+    'Dinámica': ['Villalobos Llamas Gilberto'],
+    'Tecnologia De Los Materiales': ['Cárdenas Rodriguez Selene'],
+    'Circutos 1': ['Jardines González Arturo Iván'],
+    'Electrónica': ['Mora Quiñones Jesús Uriel'],
+    'Ingles': ['Benavides Sánchez Luis Daniel'],
+    'Orientación Educativa': ['Valdivia Carvajal Dafnis'],
+}
